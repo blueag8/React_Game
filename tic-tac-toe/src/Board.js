e75a5f9853feb03d196f776668e4f47a1f8b092d
Naomi Wickham <ns.wickham08@outlook.com>
2020-05-02 13:01:20 +0000

    added react game

---
+++ tic-tac-toe/src/Board.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="game">
+            <div className="Board">
+            <Board />
+            </div>
+            <div className="Game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Board;
